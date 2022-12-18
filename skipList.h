3e62b7a3ec9400ccb0c7fbe7dd8e1e2419d82264
VikiIvanova <viki.ivanova756971@gmail.com>
2022-12-18 15:55:02 +0200

    New functions in public part for skipList

--- skipList.h
+++ skipList.h
@@ -11,9 +11,34 @@ class skipList
        Box* next;
        Box* skip;
 
-       Box(const T& data, Box* next, Box* skip);
-    }
+       Box(const T& _data, Box* _next):
+           data {_data}, next {_next}, skip {nullptr} {}
+    };
+
+    Box* start;
+    size_t size;
+
+    void clear();
+    void copy(const skipList& other);
+
+    public:
+    skipList();
+    skipList(const skipList& other);
+    ~skipList();
+
+    skipList& operator= (const skipList& other);
+
+    size_t getSize() const;
+    size_t getNumSkipElemensts() const;
     
+    Box* locate(const T& element) const;
+    bool member (const T& element) const;
+    void optimize();
+    void addElementAt(const T& element, Box*& at);
+    void addElement(const T& element);
+
+    void printSkipList() const;
+    void printSkipping() const;
 
 };
 
